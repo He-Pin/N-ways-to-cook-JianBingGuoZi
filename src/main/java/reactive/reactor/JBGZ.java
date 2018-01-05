912607ed9ba098e1c5c37eb369b26f3353f00405
虎鸣 <hepin.p@alibaba-inc.com>
2018-01-05 16:01:56 +0800

    using subscribe on

--- src/main/java/reactive/reactor/JBGZ.java
+++ src/main/java/reactive/reactor/JBGZ.java
@@ -73,9 +73,9 @@ public class JBGZ {
 //            fluxs.add(生菜Flux.cast(食材.class));
 //            fluxs.add(火腿肠Flux.cast(食材.class));
 
-            fluxs.add(鸡蛋煎饼Flux.cast(食材.class).publishOn(SCHEDULER));
-            fluxs.add(生菜Flux.cast(食材.class).publishOn(SCHEDULER));
-            fluxs.add(火腿肠Flux.cast(食材.class).publishOn(SCHEDULER));
+            fluxs.add(鸡蛋煎饼Flux.cast(食材.class).subscribeOn(SCHEDULER));
+            fluxs.add(生菜Flux.cast(食材.class).subscribeOn(SCHEDULER));
+            fluxs.add(火腿肠Flux.cast(食材.class).subscribeOn(SCHEDULER));
             return Flux.zip(fluxs, 1, (Function<Object[], 煎饼果子>) 食材List -> {
                 鸡蛋煎饼 鸡蛋煎饼一张 = (鸡蛋煎饼) 食材List[0];
                 生菜 生菜若干 = (生菜) 食材List[1];
--- src/main/java/reactive/rxjava/JBGZ.java
+++ src/main/java/reactive/rxjava/JBGZ.java
@@ -69,12 +69,12 @@ public class JBGZ {
         Flowable<煎饼果子> 煎饼果子Flux = reqFlux.flatMap(req -> {
             System.out.println(req);
             List<Flowable<食材>> fluxs = new ArrayList<>();
-//            fluxs.add(鸡蛋煎饼Flux.cast(食材.class).observeOn(SCHEDULER));
-//            fluxs.add(生菜Flux.cast(食材.class).observeOn(SCHEDULER));
-//            fluxs.add(火腿肠Flux.cast(食材.class).observeOn(SCHEDULER));
-            fluxs.add(鸡蛋煎饼Flux.cast(食材.class));
-            fluxs.add(生菜Flux.cast(食材.class));
-            fluxs.add(火腿肠Flux.cast(食材.class));
+            fluxs.add(鸡蛋煎饼Flux.cast(食材.class).subscribeOn(SCHEDULER));
+            fluxs.add(生菜Flux.cast(食材.class).subscribeOn(SCHEDULER));
+            fluxs.add(火腿肠Flux.cast(食材.class).subscribeOn(SCHEDULER));
+//            fluxs.add(鸡蛋煎饼Flux.cast(食材.class));
+//            fluxs.add(生菜Flux.cast(食材.class));
+//            fluxs.add(火腿肠Flux.cast(食材.class));
             return Flux.zip(fluxs, 1, (Function<Object[], 煎饼果子>) 食材List -> {
                 鸡蛋煎饼 鸡蛋煎饼一张 = (鸡蛋煎饼) 食材List[0];
                 生菜 生菜若干 = (生菜) 食材List[1];
